0b70b15c5512ab074c9a8466df9972d9d394ffe4
joeyrlee <jleepianist@gmail.com>
2017-04-15 00:39:21 -0700

    (final-update) Generalized folder structure with basic babel/css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,20 @@
 module.exports = {
+	entry: './src/app.js',
+	output: {
+		filename: './dist/bundle.js'
+	},
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: { 
+          presets: [ 
+            [ 'es2015', { modules: false } ] 
+          ] 
+        }
+      },
       {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
