cd60b4258e625e3c340b340c97c1ad48299f708a
joeyrlee <jleepianist@gmail.com>
2017-04-18 22:56:15 -0700

    (final) Clarified difference in scoping between es5/es6 import/exports

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
+/* JavaScript Entry File */
 require('./styles.css');
-/* Additional imports/requires here */
-
-/* JavaScript Entry File Here */
+//es6 imports from other modules (with the exception of node_modules libraries) must
+//additionally be assigned to the global `window` object here or in their respective modules 
+//or you must use es5 myVar = require('./myModule.js') syntax without `var`, `let`, or `const` assignment
+//as this automatically assigns the variable to the global scope
+//(the variables will be privately scoped and inaccessible inside of your bundle.js otherwise)
